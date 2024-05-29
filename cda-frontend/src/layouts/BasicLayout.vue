01fb1625cbf46eca53286c08ccc2b82283e07080
WaterBird <lcc1105897090@126.com>
2024-05-30 02:49:34 +0800

    init

---
+++ cda-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 100vh">
+      <a-layout-header class="header">Header</a-layout-header>
+      <a-layout-content class="content">Content</a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/Knight777777777777" target="_blank"> Created by Knight </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped></style>
