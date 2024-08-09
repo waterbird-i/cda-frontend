e918d0be4eee6fe1dd447acc50c9c8aa3e7482fa
liancong <lcc1105897090@126.com>
2024-08-09 11:36:46 +0800

    新增应用统计分析页

--- cda-frontend/src/views/app/AppStatisticPage.vue
+++ cda-frontend/src/views/app/AppStatisticPage.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="appStatisticPage">
-    <v-chart :option="appAnswerCountOptions" style="height: 300px" />
+    <v-chart
+      ref="chartRef"
+      :option="appAnswerCountOptions"
+      style="height: 800px"
+      @click="onChartClick"
+    />
     <div class="searchBar">
       <a-input-search
-        :style="{ width: '320px' }"
+        :style="{ width: '320px', marginBottom: '16px' }"
         placeholder="输入 appId"
         button-text="搜索"
         search-button
@@ -25,15 +30,34 @@ import {
 } from "@/api/appStatisticController";
 import message from "@arco-design/web-vue/es/message";
 import * as echarts from "echarts/core";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+} from "echarts/components";
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
 
 const appAnswerCountList = ref<API.AppAnswerCountDTO[]>([]);
 const appAnswerResultCountList = ref<API.AppAnswerResultCountDTO[]>([]);
+const chartRef = ref<{
+  chart: echarts.ECharts;
+} | null>(null);
 /**
  * 获取应用答题统计
  */
 const loadAppAnswerCountData = async () => {
   const res: any = await getAppAnswerCountUsingGet();
-  if (res.data.data === 0) {
+  if (res.data.code === 0) {
     appAnswerCountList.value = res.data.data || [];
   } else {
     message.error("获取数据失败" + res.data.message);
@@ -48,7 +72,7 @@ const loadAppAnswerResultCountData = async (appId: string) => {
   const res: any = await getAppAnswerResultCountUsingGet({
     appId: appId as any,
   });
-  if (res.data.data === 0) {
+  if (res.data.code === 0) {
     appAnswerResultCountList.value = res.data.data || [];
   } else {
     message.error("获取数据失败" + res.data.message);
@@ -57,7 +81,7 @@ const loadAppAnswerResultCountData = async (appId: string) => {
 
 watchEffect(() => {
   loadAppAnswerCountData();
-  loadAppAnswerResultCountData("");
+  loadAppAnswerResultCountData("3");
 });
 // 统计选项
 const appAnswerCountOptions = computed(() => {
@@ -102,6 +126,11 @@ const appAnswerCountOptions = computed(() => {
       {
         type: "bar",
         showBackground: true,
+        barMaxWidth: 160, // 设置每个柱的最大宽度
+        label: {
+          show: true,
+          position: "top",
+        },
         itemStyle: {
           color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
             { offset: 0, color: "#83bff6" },
@@ -123,6 +152,30 @@ const appAnswerCountOptions = computed(() => {
     ],
   };
 });
+// Enable data zoom when user click bar.
+
+const onChartClick = (params: any) => {
+  const zoomSize = 4; // 设置缩放大小
+  const chartInstance = chartRef.value?.chart;
+  const data = appAnswerCountList.value.map((item) => item.appId);
+  if (chartInstance) {
+    const startValue = data[Math.max(params.dataIndex - zoomSize / 2, 0)];
+    const endValue =
+      data[
+        Math.min(
+          params.dataIndex + zoomSize / 2,
+          appAnswerCountList.value.length - 1
+        )
+      ];
+    // 触发缩放
+    chartInstance.dispatchAction({
+      type: "dataZoom",
+      startValue,
+      endValue,
+    });
+  }
+};
+
 const appAnswerResultCountOptions = computed(() => {
   return {
     title: {
@@ -134,17 +187,38 @@ const appAnswerResultCountOptions = computed(() => {
       formatter: "{a} <br/>{b} : {c} ({d}%)",
     },
     legend: {
-      orient: "vertical",
+      orient: "horizontal",
       left: "left",
+      data: appAnswerResultCountList.value.map((item) => item.resultName),
     },
     series: [
       {
+        name: `应用ID`,
         type: "pie",
         radius: "55%",
         center: ["50%", "60%"],
         data: appAnswerResultCountList.value.map((item) => {
           return {
-            value: item.resultCount,
+            value: parseInt(item.resultCount || "0"),
+            name: item.resultName,
+          };
+        }),
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+      {
+        name: `应用ID${appAnswerResultCountList.value[0]}`,
+        type: "pie",
+        radius: "55%",
+        center: ["50%", "60%"],
+        data: appAnswerResultCountList.value.map((item) => {
+          return {
+            value: parseInt(item.resultCount || "0"),
             name: item.resultName,
           };
         }),
